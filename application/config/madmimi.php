e4a1991b03064266b2aa31e14859ffc799f5e705
Brett DeWoody <brett@digitalwaxworks.com>
2010-09-16 00:29:59 -0600

    Added the library and config files

---
+++ application/config/madmimi.php
@@ -0,0 +1,20 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Mad Mimi for CodeIgniter
+ *
+ * Integrates the Mad Mimi for PHP library into CodeIgniter
+ *
+ * @package		Madmimi
+ * @author		Brett DeWoody (http://www.DigitalWaxWorks.com)
+ * @version		1.0.0
+ * @based on	Mad Mimi for PHP by Nicholas Young <nicholas@madmimi.com> ...and a host of contributors. (http://github.com/madmimi/madmimi-php)
+ * @license		MIT License
+ */
+ 
+$config['madmimi_api'] = 'YOUR MAD MIMI API KEY';
+$config['madmimi_email'] = 'YOUR MAD MIMI ACCOUNT EMAIL';
+$config['madmimi_debug'] = FALSE;
+
+/* End of file madmimi.php */
+/* Location: ./application/config/madmimi.php */
